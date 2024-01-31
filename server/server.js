e7c3e40901af3e5c16c2369640a754cbeee057b4
jody isaguirre <jodyisaguirre@jodys-MacBook-Pro.local>
2024-01-31 14:31:22 -0700

    added post route handling

--- server/server.js
+++ server/server.js
@@ -31,4 +31,10 @@ app.get("/",(req,res)=>{
 app.get("/users",(req,res)=>{
     res.json(users)
 })
+
+app.post("/users",(req,res) => {
+    const userToBeAdded = req.body
+    users.push(userToBeAdded)
+    res.status(201).send (users)
+})
 app.listen(5001, () => { console.log("Server Started on Port 5000")})
