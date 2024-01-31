e8f60cae250fe2723f276fe8d40aeb4787179c64
jody isaguirre <jodyisaguirre@jodys-MacBook-Pro.local>
2024-01-31 14:23:55 -0700

    added users to server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+const users = [
+    {
+      name: "John",
+      age: 30,
+      ID: 1
+    },
+    {
+      name: "Alice",
+      age: 25,
+      ID: 2
+    },
+    {
+      name: "Bob",
+      age: 35,
+      ID: 3
+    }
+  ];
+
+app.use(cors())
+app.use(express.json())
+  
 
 app.get("/",(req,res)=>{
     res.send("Server is running")
 })
 
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
 app.listen(5001, () => { console.log("Server Started on Port 5000")})
