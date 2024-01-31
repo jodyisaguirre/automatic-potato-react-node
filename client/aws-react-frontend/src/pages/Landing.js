6199e6e7ed8361d3dd5f2fae566b9706145e4f29
jody isaguirre <jodyisaguirre@jodys-MacBook-Pro.local>
2024-01-31 15:44:09 -0700

    added frontend state management

--- client/aws-react-frontend/src/pages/Landing.js
+++ client/aws-react-frontend/src/pages/Landing.js
@@ -6,6 +6,10 @@ const Landing = () => {
     const [data, setData] = useState(null)
     const [error, setError] = useState(null)
     const [name, setName] = useState("")
+    const [age, setAge] = useState("")
+    const [id, setId] = useState("")
+
+
  
 
     const fetchData = async () => {
@@ -32,6 +36,32 @@ const Landing = () => {
         setName(e.target.value)
         console.log(e.target.value)
 
+
+    }
+    const handleAgeChange = (e) =>{
+        setAge(e.target.value)
+        console.log(e.target.value)
+        console.log(data)
+
+    }
+
+   
+
+    const handleAddOnSubmit= (e) => {
+        e.preventDefault()
+        setData([...data,{"name":name,"age":age,"id":4}])
+        
+    }
+    
+    const handleIdChange = (e) =>{
+        setId(e.target.value)
+    }
+
+    const handleDelete = () => {
+        const filterPersonById = data.filter(person => person.id != id)
+        setData(filterPersonById)
+        console.log(filterPersonById)
+        setId("")
     }
 
     if(!data){
@@ -45,8 +75,20 @@ const Landing = () => {
     return(
         <>
          <h1>You are now Reacting </h1>
-        {data.map(person => <p key={person.id}>{person.name}</p>)}
+        {data.map((person,index) => 
+        < div key={index}>
+        <p key={person.name + index}>{person.name}</p>
+        <p key={person.id + index}>{person.id}</p>
+        </div>
+
+        )}
+        <form onSubmit={handleAddOnSubmit}>
         <input value={name} placeholder='name' onChange={handleNameChange}></input>
+        <input value={age} placeholder='age' onChange={handleAgeChange}></input>
+        <button type='submit'>Add Person</button>
+        </form>
+        <input value={id} placeholder='Delete Person By ID' onChange={handleIdChange}></input>
+        <button onClick={handleDelete}>Delete Person By Id</button>
         </>
        
 
