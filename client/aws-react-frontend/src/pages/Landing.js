dfc5b6e05184c2bcff2d502bbd7ad71130742af3
jody isaguirre <jodyisaguirre@jodys-MacBook-Pro.local>
2024-01-31 15:11:52 -0700

    added name input

--- client/aws-react-frontend/src/pages/Landing.js
+++ client/aws-react-frontend/src/pages/Landing.js
@@ -1,22 +1,54 @@
 import React , {useState,useEffect} from 'react'
 
 const Landing = () => {
-    const [data,setData]=useState(null)
     const url ="http://localhost:5001/users"
 
+    const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
+    const [name, setName] = useState("")
+ 
+
     const fetchData = async () => {
-        const response = await fetch(url)
+       try{ const response = await fetch(url)
+        if(!response.ok){
+            throw new Error(`${response.status}`)
+        }
         const data = await response.json()
         setData(data)
-        console.log(data)
+        }
+        catch(error){
+            setError(error.message)
+            console.error('Error',error)
+
+        }
     }
+    
     useEffect(()=> {
         fetchData()
 
     },[])
 
+    const handleNameChange = (e) =>{
+        setName(e.target.value)
+        console.log(e.target.value)
+
+    }
+
+    if(!data){
+        return <p>loading</p>
+    }
+
+    if(error){
+        return <p>{error}</p>
+    }
+
     return(
-        <h1>You are now Reacting </h1>
+        <>
+         <h1>You are now Reacting </h1>
+        {data.map(person => <p key={person.id}>{person.name}</p>)}
+        <input value={name} placeholder='name' onChange={handleNameChange}></input>
+        </>
+       
 
     )
 }
