5dc4c1235b880e14864e2f9266a43ff3426f0bd0
jody isaguirre <jodyisaguirre@jodys-MacBook-Pro.local>
2024-02-01 07:12:13 -0700

    added amplify login

--- client/aws-react-frontend/src/App.js
+++ client/aws-react-frontend/src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator} from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import Landing from './pages/Landing';
+
+Amplify.configure(awsconfig)
 
 function App() {
 
-  
   return (
     <div className="App">
-      <Landing/>
+         <Authenticator>
+            {({ signOut, user }) => (
+              <main>
+                <div className="App">
+                  <header className="App-header">
+                  <button onClick={signOut}>Sign out</button>
+                  <Landing/>
+                  </header>
+                </div>
+              </main>
+            )}
+          </Authenticator>
+  
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
