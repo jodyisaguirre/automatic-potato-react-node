be110f8944892d8c0b42489b33e4c443a2ba2a98
jioverturelaw <jody@overture.law>
2024-09-03 15:31:57 -0600

    added stuff

--- client/aws-react-frontend/src/App.js
+++ client/aws-react-frontend/src/App.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import './App.css';
-import {Amplify} from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { withAuthenticator} from '@aws-amplify/ui-react';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// import {Amplify} from 'aws-amplify';
+// import awsconfig from './aws-exports';
+// import { withAuthenticator} from '@aws-amplify/ui-react';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
 import Landing from './pages/Landing';
 
-Amplify.configure(awsconfig)
+// Amplify.configure(awsconfig)
 
 function App() {
 
   return (
     <div className="App">
-         <Authenticator>
+         {/* <Authenticator>
             {({ signOut, user }) => (
               <main>
                 <div className="App">
                   <header className="App-header">
-                  <button onClick={signOut}>Sign out</button>
+                  <button onClick={signOut}>Sign out</button> */}
                   <Landing/>
-                  </header>
+                  {/* </header>
                 </div>
               </main>
             )}
-          </Authenticator>
+          </Authenticator> */} */}
   
     </div>
   );
 }
 
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
+export default App;
+
